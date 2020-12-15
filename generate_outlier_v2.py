311b172a95788a7034e032e19dae8ab5f1118749
Shayangnala <linghui.han@gmail.com>
2020-12-15 23:09:10 +0800

    refactored working version without multiprocessing

--- generate_outlier_v2.py
+++ generate_outlier_v2.py
@@ -146,112 +146,43 @@ with open(SYMBOL_SHORT_SAMPLE_FILE) as symbol_csv_file:
 		list_of_patents.append(my_patent)
 
 
-# Step3. Construct a vector for each patent
+def compute(i):
+	this_p = list_of_patents[i]
+
+	j = i - 1
+	while j >= 0:
+		that_p = list_of_patents[j]
+		
+		this_d = this_p.appDate
+		that_d = that_p.appDate
+
+		isAdj = False
+		if isBefore(this_d, that_d):
+			isAdj = isAdjacent(this_p, that_p)
+
+		if isAdj == True:
+			this_p.adj_list.append(that_p.appNo)
+
+		# move the pointer for the inner while loop
+		j = j - 1
 
 
-# Step4. For each patent, compare it against a range of patents and determine 
-# if it is an outlier or not. 
+i = 216 # 216 for nationality 1, 2358 for nationality 0
+while i < len(list_of_patents):
+	compute(i)
+	# move the pointer for the outer while loop
+	i = i + 1
 
-# data structure to store outlier patents
 
-# compare and compute the distance between pairs
-# for i, this_k in enumerate(result_dict):
-# 	# i is the index starting from 0
-# 	# k is key
-# 	# the focal item
-# 	this_bitstring = result_dict[k]['bitstring']
-# 	# print ("focal string: ", this_bitstring)
-# 	print ("this key: ", this_k)
-
-# 	for that_k in list(result_dict.keys())[i+1:]:
-# 		print ("that key: ", that_k)
-# 		that_bitstring = result_dict[that_k]['bitstring']
-# 		isAdj = isAdjacent(this_bitstring, that_bitstring)
-
-# 		if isAdj == True:
-# 			adj_dict[this_k].append(that_k)
-# 			adj_dict[that_k].append(this_k)
-
-# 	print ("================")
-# for i, this_p in enumerate(list_of_patents):
-
-# 	for j in range(i+1, len(list_of_patents)):
-
-# 		that_p = list_of_patents[j]
-
-# 		# compare the date
-# 		this_d = this_p.appDate
-# 		that_d = that_p.appDate
-
-# 		isBefore(this_d, that_d)
-
-# 		isAdj = isAdjacent(this_p, that_p)
-
-# 		if isAdj == True:
-# 			this_p.adj_list.append(that_p)
-# 			that_p.adj_list.append(this_p)
-
-#i = 1
+# seperate write module
 with open(TEST_OUTPUT, 'w') as output_csv:
 	writer = csv.writer(output_csv)
 	# write header row
 	writer.writerow(["appNo", "appDate", "Adj Patents"])
 
-	i = 216 # 216 for nationality 1, 2358 for nationality 0
-	while i < len(list_of_patents):
-		this_p = list_of_patents[i]
-
-		j = i - 1
-		adj_list = []
-		while j >= 0:
-			that_p = list_of_patents[j]
-			
-			this_d = this_p.appDate
-			that_d = that_p.appDate
-
-			isAdj = False
-			if isBefore(this_d, that_d):
-				isAdj = isAdjacent(this_p, that_p)
-
-			if isAdj == True:
-				this_p.adj_list.append(that_p)
-				adj_list.append(str(that_p.appNo))
-
-			# move the pointer for the inner while loop
-			j = j - 1
-
+	for i in range (216, len(list_of_patents)):
+		p = list_of_patents[i]	
 		# write to a file
-		writer.writerow([this_p.appNo, this_p.appDate, " ".join(adj_list)])
-
-		# move the pointer for the outer while loop
-		i = i + 1
-
-# print ("02101189: ", result_dict['02101189']['bitstring'])
-# print ("02101190: ", result_dict['02101190']['bitstring'])
-
-# print ("test test test: ", isAdjacent(result_dict['02101189']['bitstring'], result_dict['02101190']['bitstring']))
-
-# write results to file
-# with open(TEST_OUTPUT, 'w') as output_csv:
-# 	writer = csv.writer(output_csv)
-# 	# write header row
-# 	writer.writerow(["appNo", "appDate", "Adj Patents"])
-
-for i in range (216, len(list_of_patents)):
-	p = list_of_patents[i]
-
-	print ("debug 2: ", p.appNo, " ", len(p.adj_list))
-		# writer.writerow([p.appNo, p.appDate, " ".join(adj_list)])
-
-
-# outlier_patents = []
-# for p in list_of_patents:
-# 	if len(p.adj_list) > 0:
-# 		print (p.appNo, " ", str(p.adj_list[0]))
-# 	if len(p.adj_list) == 0:
-# 		outlier_patents.append(p)
-
-# print ("=========The following are outliers==============")
-# for p in outlier_patents:
-# 	print (p.appNo)
+		writer.writerow([p.appNo, p.appDate, " ".join(p.adj_list)])
+		print ("debug 2: ", p.appNo, " ", " ".join(p.adj_list))
 
