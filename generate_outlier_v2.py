546beae93791dd498aa363ab56374f468a624df1
Shayangnala <linghui.han@gmail.com>
2020-12-17 11:30:39 +0800

    20201217 working version with multiprocessing

--- generate_outlier_v2.py
+++ generate_outlier_v2.py
@@ -2,14 +2,14 @@ import csv
 import datetime
 import multiprocessing
 
-# no dict version
-
-# Step2. Load a sample data file
-SYMBOL_SHORT_SAMPLE_FILE = "/Users/shayangnala/py_crawler/outlier_patent/ipcs_sample_till_2006_nationality_1.csv"
+SYMBOL_SHORT_SAMPLE_FILE = "/Users/shayangnala/py_crawler/outlier_patent/ipcs_sample_till_2006_nationality_0.csv"
 MAIN_GROUP_LIST_FILE = "/Users/shayangnala/Downloads/maingroup_symbol_list.csv"
 TEST_OUTPUT = "/Users/shayangnala/py_crawler/outlier_patent/test_output.csv"
 
+main_group_symbol_list = []
+list_of_patents = []
 
+# the patent class
 class Patent:
 
 	def __init__(self, appNo, appDate, bitstring):
@@ -18,13 +18,10 @@ class Patent:
 		self.bitstring = bitstring
 		self.adj_list = []
 
-
-
 	def __str__(self):
 		return "appNo: " + str(self.appNo) + "appDate: " + str(self.appDate)
 		# return "i am a string"
 
-main_group_symbol_list = []
 def retrieve_main_group_symbol_list():
 	with open(MAIN_GROUP_LIST_FILE) as maingroup_list_csv:
 		reader = csv.reader(maingroup_list_csv)
@@ -33,28 +30,11 @@ def retrieve_main_group_symbol_list():
 
 
 
-# this function convert a list to a string
+# this function converts a list to a string
 def convert_list_to_string(bit_list):
 	return ''.join(bit_list)
 
-# if two bit strings are adjacent
-def isAdjacent(bitstring_1, bitstring_2):
-	num_diff_bits = 0
-	if len(bitstring_1) != len(bitstring_2):
-		print ("Exception: two bit strings have different lengths ", len(bitstring_1), " ", len(bitstring_2))
-		return
-
-
-	for i in range (0, len(bitstring_1)):
-		if bitstring_1[i] != bitstring_2[i]:
-			# print ("pos: " , i, " bit from string1: ", bitstring_1[i], " bit from string2: ", bitstring_2[i])
-			num_diff_bits = num_diff_bits + 1
-
-	# print ("Different bits between the two bitstrings: ", num_diff_bits)
-	isAdj = num_diff_bits < 2
-
-	return isAdj
-
+# if two patents are adjcent
 def isAdjacent(this_p, that_p):
 	num_diff_bits = 0
 	bitstring_1 = this_p.bitstring
@@ -89,23 +69,15 @@ def isBefore(this_d, that_d):
 
 	return d1 > d2
 
-result_dict = {}
 
-list_of_patents = []
+#=======The main functions======
 retrieve_main_group_symbol_list()
 with open(SYMBOL_SHORT_SAMPLE_FILE) as symbol_csv_file:
 	reader = csv.reader(symbol_csv_file)
 	header_row = next(reader) # reader the header row
 
 	# construct bitstring according to the main_group_symbol_list	
-
 	for row in reader:
-
-		# # initialize the dictionary
-		# main_group_dict = {}
-		# for k in main_group_symbol_list:
-		# 	main_group_dict[k] = '0'
-
 		# initialize a list
 		n_list = ['0']*len(main_group_symbol_list)
 
@@ -119,10 +91,6 @@ with open(SYMBOL_SHORT_SAMPLE_FILE) as symbol_csv_file:
 				n_list[pos] = '1'
 			else: 
 				print (main_group, " not in the registered list")
-			# if main_group in main_group_dict:
-			# 	main_group_dict[main_group] = '1'
-			# else: 
-			# 	print ("main group not included in the dictionary: ", main_group)
 
 		# == the end of creating the bit string
 
@@ -133,14 +101,6 @@ with open(SYMBOL_SHORT_SAMPLE_FILE) as symbol_csv_file:
 		grtdate = row[4]
 		nationality = row[5]
 
-		# store the data
-		# entry_dict = {}
-		# entry_dict['appdate'] = appdate
-		# entry_dict['grtdate'] = grtdate
-		# entry_dict['nationality'] = nationality
-		# entry_dict['bitstring'] = convert_list_to_string(n_list)
-
-		# result_dict[apn] = entry_dict
 
 		my_patent = Patent(apn, appdate, convert_list_to_string(n_list))
 		list_of_patents.append(my_patent)
@@ -169,27 +129,13 @@ def compute(i):
 	return this_p
 
 
-i = 216 # 216 for nationality 1, 2358 for nationality 0
-# while i < len(list_of_patents):
-# 	compute(i)
-# 	# move the pointer for the outer while loop
-# 	i = i + 1
-
-def change_state(l):
-	# new_p = Patent('p1', ' ', ' ')
-	# l.append(new_p)
-	# l[0] = Patent('changed', ' ', ' ') this is very useful
-	l[0].appNo = 'changed'
-
+# 216 for nationality 1, 2358 for nationality 0
 if __name__ == "__main__":
 	with multiprocessing.Manager() as manager:
 		pl = multiprocessing.Pool(4)
 
-		result = pl.map(compute, range (216, len(list_of_patents)))
+		result = pl.map(compute, range (2358, len(list_of_patents)))
 
-
-		# for p in result:
-		# 	print ("Debug 3: ", p.appNo, " ", p.adj_list)
 		with open(TEST_OUTPUT, 'w') as output_csv:
 			writer = csv.writer(output_csv)
 			# write header row
@@ -198,6 +144,7 @@ if __name__ == "__main__":
 			for p in result:
 				# write to a file
 				writer.writerow([p.appNo, p.appDate, " ".join(p.adj_list)])
-				print ("debug 2: ", p.appNo, " ", " ".join(p.adj_list))
+				# print ("debug 2: ", p.appNo, " ", " ".join(p.adj_list))
+
 
 		pl.close()
